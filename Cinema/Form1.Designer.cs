11cb227828b10909b52249088ad1e96de50d5a95
Lavie <paintjinx1@gmail.com>
2022-07-24 21:22:55 +0700

    Add project files.

---
+++ Cinema/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Cinema
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblScreen = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlSeats = new System.Windows.Forms.Panel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.tblScreen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblScreen
+            // 
+            this.tblScreen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblScreen.ColumnCount = 1;
+            this.tblScreen.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblScreen.Controls.Add(this.label1, 0, 0);
+            this.tblScreen.Location = new System.Drawing.Point(3, 12);
+            this.tblScreen.Name = "tblScreen";
+            this.tblScreen.RowCount = 1;
+            this.tblScreen.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblScreen.Size = new System.Drawing.Size(535, 76);
+            this.tblScreen.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MÀN HÌNH";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlSeats
+            // 
+            this.pnlSeats.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlSeats.Location = new System.Drawing.Point(75, 105);
+            this.pnlSeats.Name = "pnlSeats";
+            this.pnlSeats.Size = new System.Drawing.Size(421, 216);
+            this.pnlSeats.TabIndex = 1;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(75, 394);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(85, 18);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "Thành tiền";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(166, 394);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(136, 23);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(75, 459);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(85, 33);
+            this.btnSelect.TabIndex = 4;
+            this.btnSelect.Text = "Chọn";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(238, 459);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(92, 33);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Huỷ bỏ";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(670, 600);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(383, 459);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(81, 33);
+            this.btnEnd.TabIndex = 4;
+            this.btnEnd.Text = "Kết thúc";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSelect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 545);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.pnlSeats);
+            this.Controls.Add(this.tblScreen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tblScreen.ResumeLayout(false);
+            this.tblScreen.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tblScreen;
+        private Label label1;
+        private Panel pnlSeats;
+        private Label lblTotal;
+        private TextBox txtTotal;
+        private Button btnSelect;
+        private Button btnCancel;
+        private Button button3;
+        private Button btnEnd;
+    }
+}
---
+++ Cinema/Form1.cs
@@ -0,0 +1,102 @@
+﻿namespace Cinema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitSeats(3, 5);
+        }
+        private void InitSeats(int row, int col)
+        {
+            int x, y = 11, distance = 70, count = 1;
+            for(int i = 0; i < row; i++)
+            {
+                x = 16;
+                for(int j = 0; j < col; j++)
+                {
+                    Button btnSeat = new Button();
+                    btnSeat.Location = new System.Drawing.Point(x, y);
+                    btnSeat.Size = new System.Drawing.Size(50, 50);
+                    btnSeat.Text = count++.ToString();
+                    btnSeat.BackColor = Color.White;
+                    btnSeat.UseVisualStyleBackColor = true;
+                    pnlSeats.Controls.Add(btnSeat);
+                    btnSeat.Click += BtnSeat_Click;
+                    x += distance;
+                }
+                y += distance;
+            }
+        }
+
+        private void BtnSeat_Click(object? sender, EventArgs e)
+        {
+            Button btnSeat = (Button)sender;
+            if (btnSeat.BackColor == Color.Yellow)
+            {
+                MessageBox.Show("Ghế này đã bị mua, giận hết sức o(￣┰￣*)ゞ");
+                return;
+            }
+            if (btnSeat.BackColor == Color.White)
+            {
+                btnSeat.BackColor = Color.Blue;
+            }
+            else
+            {
+                btnSeat.BackColor = Color.White;
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            double Total = 0;
+            foreach (Button seat in pnlSeats.Controls)
+            {
+                if (seat.BackColor == Color.Blue)
+                {
+                   Total += BillCheck(seat.Text);
+                   seat.BackColor = Color.Yellow;
+                }
+            }
+            txtTotal.Text = Total.ToString();
+        }
+        private float BillCheck(string text) 
+        {
+            int numberSeat = Int32.Parse(text);
+            if (numberSeat <= 5)
+            {
+                return 50000;
+            }
+            else if (numberSeat <= 10)
+            {
+                return 65000;
+            }
+            else
+            {
+                return 80000;
+            }
+            return 0;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            foreach (Button seat in pnlSeats.Controls)
+            {
+                if (seat.BackColor == Color.Blue)
+                {
+                    seat.BackColor = Color.White;
+                }
+            }
+            txtTotal.Text = "0";
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
